65535cd5c1c09380b5596f3624d139a3bad7e567
Lanbo Zhang <lanbo_zhang@hotmail.com>
2021-11-26 22:00:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -67,8 +67,8 @@ def main(user, passwd, step):
         return
     
     if step == '':
-        print ("已设置为随机步数（11000-13000）")
-        step = str(random.randint(11000,13000))
+        print ("已设置为随机步数（11000-12000）")
+        step = str(random.randint(11000,12000))
     login_token = 0
     login_token,userid = login(user,password)
     if login_token == 0:
